10fc4bf4f670452b39586e3e85435b9971b393a7
datacamp-content-creator <32279578+datacamp-content-creator@users.noreply.github.com>
2019-04-27 13:38:05 +0200

    Update requirements.R from editor

--- requirements.R
+++ requirements.R
@@ -1,7 +1,7 @@
 library(remotes)
 
 ##### Install specific package versions from CRAN #####
-
+install_version("seewave","2.1.3"
 # install_version("ggplot2", "3.0.0")
 # install_version("dplyr", "0.7.5")
 # install_version("tidyr", "0.8.1")
